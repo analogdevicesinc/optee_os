e5d52df9706979e7340e3744a48eb391b19dfc4f
Greg Malysa <gmalysa@alumni.stanford.edu>
2022-03-24 17:03:12 -0400

    plat-adi: New platform support for ADSP-SC598 from Analog Devices
    Signed-off-by: Greg Malysa <gmalysa@alumni.stanford.edu>

---
+++ core/arch/arm/plat-adi/platform_config.h
@@ -0,0 +1,20 @@
+/**
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2022, Analog Devices, Inc.
+ */
+#ifndef PLATFORM_CONFIG_H
+#define PLATFORM_CONFIG_H
+
+#include <mm/generic_ram_layout.h>
+
+#define STACK_ALIGNMENT 64
+
+#define ADSP_SC598_GICD_BASE 0x31200000
+#define ADSP_SC598_GICR_BASE 0x31240000
+#define ADSP_SC598_GIC_SIZE 0x80000
+
+#define ADSP_SC598_UART0_BASE 0x31003000
+#define ADSP_SC598_UART_SIZE 0x1000
+
+#endif
