8bf9d59565e4ed577418556149bc7dadc0068863
Greg Malysa <malysagreg@gmail.com>
2022-05-12 20:15:42 -0400

    plat-adi: Initial support for OTP and HUK
    Add OTP pTA. This handles memory management and forwards calls to the
    OTP blob that is built separately.
    
    Additionally, adds HUK that programs and returns a randomly generated
    key if requested.
    
    Signed-off-by: Greg Malysa <malysagreg@gmail.com>
    Signed-off-by: UtsavAgarwalADI <utsav.agarwal@analog.com>

--- core/arch/arm/plat-adi/conf.mk
+++ core/arch/arm/plat-adi/conf.mk
@@ -1,5 +1,6 @@
 PLATFORM_FLAVOR ?= adsp_sc598
 
+ifeq ($(PLATFORM_FLAVOR), adsp_sc598)
 $(call force,CFG_ARM64_core,y)
 include core/arch/arm/cpu/cortex-armv8-0.mk
 supported-ta-targets = ta_arm64
@@ -8,6 +9,9 @@ $(call force,CFG_ARM_GICV3,y)
 $(call force,CFG_WITH_LPAE,y)
 $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 $(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+else
+$(error Unsupported PLATFORM_FLAVOR "$(PLATFORM_FLAVOR)")
+endif
 
 $(call force,CFG_CORE_ASLR,n)
 
@@ -19,4 +23,16 @@ CFG_SHMEM_SIZE ?= 0x00f00000
 CFG_SHMEM_START ?= ($(CFG_TZDRAM_START) - $(CFG_SHMEM_SIZE))
 CFG_TEE_RAM_VA_SIZE ?= 0x00400000
 
-CFG_MMAP_REGIONS ?= 24
+CFG_MMAP_REGIONS ?= 32
+
+# OTP Configuration options. If _ALL = y then the OTP pTA makes requests as a secure
+# source. Otherwise, it makes them as a nonsecure source. You may wish to set LOCK_ALL
+# to y to allow userspace to lock the SoC for example, or INVALIDATE_ALL to allow
+# userspace to invalidate "secure" type keys (ex. secure jtag keys) in addition to
+# nonsecure type keys (ex. secure boot public keys, which are not a secret)
+CFG_ADI_OTP_READ_ALL ?= n
+CFG_ADI_OTP_WRITE_ALL ?= n
+CFG_ADI_OTP_LOCK_ALL ?= n
+CFG_ADI_OTP_INVALIDATE_ALL ?= n
+CFG_ADI_OTP_IS_VALID_ALL ?= y
+CFG_ADI_OTP_IS_WRITTEN_ALL ?= y
--- core/arch/arm/plat-adi/sub.mk
+++ core/arch/arm/plat-adi/sub.mk
@@ -1,4 +1,5 @@
 
 global-incdirs-y += .
-srcs-y += main.c
+srcs-y += main.c otp_pta.c huk.c
 
+libdeps += libotp.a
