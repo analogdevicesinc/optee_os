38141bfbe8e691f1f4156cd5d057db1155edad34
Greg Malysa <gmalysa@alumni.stanford.edu>
2022-09-14 12:15:56 -0400

    plat-adi: check if huk is still zero and issue error message but keep running
    Signed-off-by: Greg Malysa <gmalysa@alumni.stanford.edu>

--- core/arch/arm/plat-adi/huk.c
+++ core/arch/arm/plat-adi/huk.c
@@ -19,12 +19,24 @@ TEE_Result tee_otp_get_hw_unique_key(struct tee_hw_unique_key *hwkey)
 	struct adi_otp *otp = adi_get_otp();
 	uint8_t buffer[ADI_OTP_HUK_BYTE_LEN];
 	uint32_t len = sizeof(buffer);
+	bool zeroes = true;
+	int i;
 
 	result = adi_otp_read(otp, ADI_OTP_ID_huk, buffer, &len, ADI_OTP_ACCESS_SECURE);
 	if (TEE_SUCCESS != result)
 		return result;
 
 	assert(sizeof(hwkey->data) <= sizeof(buffer));
+
+	for (i = 0; i < ADI_OTP_HUK_BYTE_LEN; ++i) {
+		if (0 != buffer[i]) {
+			zeroes = false;
+			break;
+		}
+	}
+
+	if (zeroes)
+		EMSG("HUK OTP is programmed with zeroes--please program a real HUK or enable autogeneration\n");
 
 	memcpy(&hwkey->data[0], buffer, sizeof(hwkey->data));
 	return TEE_SUCCESS;
