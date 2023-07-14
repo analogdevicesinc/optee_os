6e86e3385e9c7206abe9eba1c6db4d82c379e284
Greg Malysa <gmalysa@alumni.stanford.edu>
2023-07-14 15:50:45 -0400

    plat-adi: spu: refactor to support per-platform-flavor/soc spu initializations

---
+++ core/arch/arm/plat-adi/adsp_sc598/spu.c
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/**
+ * SPU configuration for the SC598 and related SoCs
+ *
+ * Copyright (c) 2023, Analog Devices, Inc.
+ */
+#include <inttypes.h>
+#include <platform_config.h>
+#include <stddef.h>
+#include <util.h>
+
+#include "../spu.h"
+
+/**
+ * @todo consider RCU as well, this requires the reset control unit to be migrated
+ * and properly supported through ARM PSCI (power state coordination interface)
+ * @todo Consider:
+ * - SMPU L2-related registers; these should only be configurable on sharc possibly?
+ * - More of the debug registers need to be covered to prevent enabling them
+ */
+void spu_platform_init(void) {
+	size_t i;
+
+	/**
+	 * Note that the CGU, CDU, and PLL are WP only so that
+	 * they can be read by the nonsecure components
+	 */
+	uint32_t securep_ids[] = {
+		109, // DMC0
+		121, // SPU0
+		126, // DPM0
+		129, 130, 131, 132, 133, 134, // System Watchpoint Units
+		139, 140, // SWU and SMPU for DMC0
+		181, // DAP ROM
+		182, 183, 184, // SHARC0 DBG, CTI, PTM
+		186, 187, 188, // SHARC1 DBG, CTI, PTM
+		201, // TAPC
+		202, // Debug Control
+		203, 204, 205, 206, // System Watchpoint Units
+	};
+
+	/**
+	 * If these are not also secured, they can still be read by the open world
+	 * @todo linux needs some read-only support to be able to lock cgu/cdu regs
+	 * so those have been left out until the linux driver is fixed or the system
+	 * won't boot, permissible during development only
+	 */
+	uint32_t wp_ids[] = {
+		109, // DMC0
+		127, // PLL0
+		128, // PLL1
+	};
+
+	// Initially, all peripherals are marked to support non-secure transactions
+	// Here, mark the core protected peripherals as secure; other drivers can
+	// also protect their peripherals with the SPU api separately
+	for (i = 0; i < ARRAY_SIZE(securep_ids); ++i) {
+		spu_peripheral_secure(securep_ids[i]);
+	}
+
+	for (i = 0; i < ARRAY_SIZE(wp_ids); ++i) {
+		spu_peripheral_writeprotect(wp_ids[i], SPU_WP_ALL);
+	}
+
+}
