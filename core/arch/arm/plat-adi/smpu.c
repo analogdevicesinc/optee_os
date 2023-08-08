b49293ca8d66e9ab57caef9f4be84227bbde9197
Greg Malysa <malysagreg@gmail.com>
2023-08-08 15:38:37 -0400

    plat-adi: implement TRU support and SMC to trigger some masters
    Implement Trigger Routing Unit (TRU) support for ADI SC5XX platforms
    to enable event routing between hardware sources and targets. Add SMC
    interface for non-secure triggers.
    
    Key features:
    - TRU driver with connect/trigger functionality
    - SMC interface for permitted non-secure trigger operations
    - Platform-specific configuration and initialization
    - Security restrictions on trigger sources
    
    Signed-off-by: Greg Malysa <malysagreg@gmail.com>
    Signed-off-by: UtsavAgarwalADI <utsav.agarwal@analog.com>

--- core/arch/arm/plat-adi/smpu.c
+++ core/arch/arm/plat-adi/smpu.c
@@ -15,42 +15,42 @@
 #include <trace.h>
 #include <util.h>
 
-register_phys_mem(MEM_AREA_IO_SEC, ADSP_SC5XX_SMPU9_BASE, ADSP_SC5XX_SMPU9_SIZE);
+#include "smpu.h"
 
 #define SMPU_REGION_REGS_SIZE	0x18
 #define SMPU_MAX_REGIONS		8
 
-#define SMPU_RCTL(n)		(0x20 + SMPU_REGION_REGS_SIZE*(n))
+#define SMPU_RCTL(n)		(0x20 + SMPU_REGION_REGS_SIZE * (n))
 #define SMPU_RCTL_WPROTEN	BIT(10)
 #define SMPU_RCTL_RPROTEN	BIT(8)
 #define SMPU_RCTL_EN		BIT(0)
 
-#define SMPU_RADDR(n)		(0x24 + SMPU_REGION_REGS_SIZE*(n))
-#define SMPU_RIDA(n)		(0x28 + SMPU_REGION_REGS_SIZE*(n))
-#define SMPU_RIDMASKA(n)	(0x2c + SMPU_REGION_REGS_SIZE*(n))
-#define SMPU_RIDB(n)		(0x30 + SMPU_REGION_REGS_SIZE*(n))
-#define SMPU_RIDMASKB(n)	(0x34 + SMPU_REGION_REGS_SIZE*(n))
+#define SMPU_RADDR(n)		(0x24 + SMPU_REGION_REGS_SIZE * (n))
+#define SMPU_RIDA(n)		(0x28 + SMPU_REGION_REGS_SIZE * (n))
+#define SMPU_RIDMASKA(n)	(0x2c + SMPU_REGION_REGS_SIZE * (n))
+#define SMPU_RIDB(n)		(0x30 + SMPU_REGION_REGS_SIZE * (n))
+#define SMPU_RIDMASKB(n)	(0x34 + SMPU_REGION_REGS_SIZE * (n))
 
-#define SMPU_SECURERCTL(n)	(0x820 + 4*(n))
+#define SMPU_SECURERCTL(n)	(0x820 + 4 * (n))
 
 /**
  * We use the secure access settings to restrict regions based on the secure/insecure
  * state of the master accessing the region, rather than doing ID based comparisons
  * because the IDs do not extend to core secure/insecure options
  */
-static void smpu_configure_region(vaddr_t smpu_base, uint32_t id, uint32_t base,
-	uint32_t size)
+void smpu_configure_region(vaddr_t smpu_base, uint32_t id, uint32_t base,
+			   uint32_t size)
 {
 	uint32_t aligned_base;
 
 	// Size must be a power of 2
-	if (size & (size-1)) {
+	if (size & (size - 1)) {
 		EMSG("Invalid size 0x%x specified for smpu\n", size);
 		panic();
 	}
 
 	// Regions are size-aligned, not page aligned!
-	aligned_base = base & ~(size-1);
+	aligned_base = base & ~(size - 1);
 	if (base != aligned_base) {
 		EMSG("SMPU region base must be aligned as a multiple of the region size\n");
 		panic();
@@ -62,7 +62,7 @@ static void smpu_configure_region(vaddr_t smpu_base, uint32_t id, uint32_t base,
 
 	if (id < SMPU_MAX_REGIONS) {
 		DMSG("configuring region %u [0x%x, 0x%x] for secure-only transactions\n", id,
-			base, base + (1 << (size + 12)) - 1);
+		     base, base + (1 << (size + 12)) - 1);
 
 		io_write32(smpu_base + SMPU_RADDR(id), base);
 		// 0 = secure enabled, non-secure disabled
@@ -77,11 +77,12 @@ static void smpu_configure_region(vaddr_t smpu_base, uint32_t id, uint32_t base,
  * because the SMPU requires strict size based alignment (i.e. 16 MB must be
  * 16 MB aligned, not 4 KB aligned)
  */
-static uint32_t smpu_configure_compound_region(vaddr_t smpu_base, uint32_t id,
-	uint32_t base, uint32_t size)
+uint32_t smpu_configure_compound_region(vaddr_t smpu_base, uint32_t id,
+					uint32_t base, uint32_t size)
 {
 	while (size && id < SMPU_MAX_REGIONS) {
 		uint32_t rsize = (1 << __builtin_ctz(size));
+
 		smpu_configure_region(smpu_base, id, base, rsize);
 		id += 1;
 		base += rsize;
@@ -99,31 +100,6 @@ static uint32_t smpu_configure_compound_region(vaddr_t smpu_base, uint32_t id,
 
 static TEE_Result smpu_init(void)
 {
-	uint32_t tzbase, tzsize;
-	uint32_t id;
-	vaddr_t smpu_base = core_mmu_get_va(ADSP_SC5XX_SMPU9_BASE, MEM_AREA_IO_SEC,
-		ADSP_SC5XX_SMPU9_SIZE);
-
-	if (!smpu_base)
-		panic();
-
-	/*
-	 * Peripheral restrictions are generally covered through the SPU
-	 * So we only configure the SMPU for DMC0 (SMPU9) to cover restricted
-	 * regions of DDR
-	 */
-	tzbase = TZDRAM_BASE;
-	tzsize = TZDRAM_SIZE;
-	id = 0;
-
-	if (TZDRAM_BASE + TZDRAM_SIZE == CFG_TFAMEM_START) {
-		tzsize += CFG_TFAMEM_SIZE;
-	} else {
-		id = smpu_configure_compound_region(smpu_base, id,
-			CFG_TFAMEM_START, CFG_TFAMEM_SIZE);
-	}
-
-	smpu_configure_compound_region(smpu_base, id, tzbase, tzsize);
-	return TEE_SUCCESS;
+	return smpu_platform_init();
 }
 early_init(smpu_init);
