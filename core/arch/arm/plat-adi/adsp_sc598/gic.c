1a8366e7ffc050041fffbb7a5fdd734b68b9d85c
Oliver Gaskell <Oliver.Gaskell@analog.com>
2024-08-29 11:15:48 +0100

    plat-adi: Update GIC initialisation to match new API
    Signed-off-by: Oliver Gaskell <Oliver.Gaskell@analog.com>

--- core/arch/arm/plat-adi/adsp_sc598/gic.c
+++ core/arch/arm/plat-adi/adsp_sc598/gic.c
@@ -12,24 +12,9 @@
 #include <platform_config.h>
 #include <trace.h>
 
-static struct gic_data gic_data __nex_bss;
-
 register_phys_mem(MEM_AREA_IO_SEC, ADSP_SC5XX_GICD_BASE, ADSP_SC5XX_GIC_SIZE);
 
-void itr_core_handler(void) {
-	gic_it_handle(&gic_data);
-}
-
-void main_init_gic(void) {
-	vaddr_t gicd_base;
-
-	gicd_base = core_mmu_get_va(ADSP_SC5XX_GICD_BASE, MEM_AREA_IO_SEC,
-		ADSP_SC5XX_GIC_SIZE);
-
-	if (!gicd_base)
-		panic();
-
-	/* Initialize GIC */
-	gic_init(&gic_data, 0, gicd_base);
-	itr_init(&gic_data.chip);
+void main_init_gic(void)
+{
+	gic_init(0, ADSP_SC5XX_GICD_BASE);
 }
